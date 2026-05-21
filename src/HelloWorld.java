9ad395e44954475ba77e47393b5e950f20c2aed4
SRILUXMANAN Seevaghan <12510499@s30415.iutv.univ-paris13.fr>
2026-05-21 11:46:45 +0200

    modif

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,9 @@
-
 public class HelloWorld {
-
+ 
 	public static void main(String[] args) {
 		System.out.println("Ciao mondo !");
-		System.out.println("Hello World !");
+		System.out.println("Olá mundo !");
 		System.out.println("Hola Mundo !");
 	}
-
+ 
 }
